ef68e63c2af899c5e8e0213fac7c9367bed89879
VeronicaFontana <fontana.veronica98@gmail.com>
2023-10-19 15:25:22 +0200

    [ADD] Images, top-bar

---
+++ src/components/Header.vue
@@ -0,0 +1,130 @@
+<script>
+export default {
+  name:"Header"
+}
+</script>
+
+<template>
+  <header>
+    <section class="top-bar">
+      <nav class="container navbar navbar-expand-lg">
+        <div class="container-fluid">
+          <div class="collapse navbar-collapse col-5 fw-bold" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Home
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="#">Action</a></li>
+                  <li><a class="dropdown-item" href="#">Another action</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+                  </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Pages
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="#">Action</a></li>
+                  <li><a class="dropdown-item" href="#">Another action</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Courses
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="#">Action</a></li>
+                  <li><a class="dropdown-item" href="#">Another action</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Features
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="#">Action</a></li>
+                  <li><a class="dropdown-item" href="#">Another action</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Blog
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="#">Action</a></li>
+                  <li><a class="dropdown-item" href="#">Another action</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Shop
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="#">Action</a></li>
+                  <li><a class="dropdown-item" href="#">Another action</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+          <div class="logo col-3 text-center">
+            <img src="../../public/dark-logo.png" alt="">
+          </div>
+          <div class="search d-flex align-items-center justify-content-end  col-4">
+            <i class="fa-solid fa-cart-shopping"></i>
+            <i class="fa-regular fa-circle-user"></i>
+            <form class="d-flex" role="search">
+              <input type="text" class="form-control" placeholder="Search..." aria-label="Search" aria-describedby="basic-addon1">
+              <span class="input-group-text" id="basic-addon1"><i class="fa-solid fa-magnifying-glass"></i></span>
+            </form>
+          </div>
+        </div>
+      </nav>
+    </section>
+    
+    <section class="jumbotron"></section>
+  </header>
+</template>
+
+<style lang="scss" scoped> 
+@use "../scss/partials/variables" as *;
+.top-bar{
+  display: flex;
+  height: 80px;
+  background-color: $background-top;
+
+  .container{
+    max-width: 1230px;
+
+    .logo{
+      img{
+        width: 50%;
+        height: 50%;
+      }
+    }
+    .search{
+      .fa-regular{
+        padding: 0 20px;
+      }
+    }  
+  }
+}
+
+.jumbotron{
+  height: 670px;
+  border: 1px solid red;
+}
+
+</style>
